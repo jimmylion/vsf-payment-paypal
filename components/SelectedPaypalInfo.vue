7cd591be89f8f3f874b513c1890e0089339d41dc
Fifciuu <filip@newfantastic.com>
2020-02-07 15:22:50 +0100

    Paypal Info component

---
+++ components/SelectedPaypalInfo.vue
@@ -0,0 +1,22 @@
+<template functional>
+  <div class="paypal-payment-step-information">
+    <img src="../assets/paypalLogo.svg" alt="PayPal" class="paypal__logo">
+    <p class="paypal__info">{{ parent.$t('Pay with PayPal on last step') }}</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .paypal-payment-step-information {
+    min-height: 180px;
+    @include flex($place: center, $direction: column);
+  }
+
+  .paypal {
+    &__logo {
+      max-width: 150px;
+    }
+    &__info {
+      margin: 30px 0;
+    }
+  }
+</style>
