d335c50040ccd79ca320a232fe91527bd44eae48
Fifciuu <filip@newfantastic.com>
2020-03-10 15:40:55 +0100

    Removed legacy action

--- store/actions.ts
+++ store/actions.ts
@@ -6,19 +6,19 @@ import { SET_FILLING, SET_PAYPAL_EXPRESS_CREDENTIALS, SET_PAYPAL_EXPRESS_USING, 
 
 // it's a good practice for all actions to return Promises with effect of their execution
 export const actions: ActionTree<PaypalState, any> = {
-  complete({ }, params) {
-    let url = config.paymentPaypalMagento2.endpoint.complete
-    url = config.storeViews.multistore ? adjustMultistoreApiUrl(url) : url
-    return fetch(url, {
-      method: 'POST',
-      mode: 'cors',
-      headers: {
-        'Accept': 'application/json, text/plain, */*',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(params)
-    }).then(resp => { return resp.json() })
-  },
+  // complete({ }, params) {
+  //   let url = config.paymentPaypalMagento2.endpoint.complete
+  //   url = config.storeViews.multistore ? adjustMultistoreApiUrl(url) : url
+  //   return fetch(url, {
+  //     method: 'POST',
+  //     mode: 'cors',
+  //     headers: {
+  //       'Accept': 'application/json, text/plain, */*',
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify(params)
+  //   }).then(resp => { return resp.json() })
+  // },
   setExpressCheckout({ }, params) {
     let url = config.paymentPaypalMagento2.endpoint.setExpressCheckout
     url = config.storeViews.multistore ? adjustMultistoreApiUrl(url) : url
